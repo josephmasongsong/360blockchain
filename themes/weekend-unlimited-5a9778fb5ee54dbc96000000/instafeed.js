4a737d42126e76eae04f2ad4eb405d0fdbfd4d80
josephmasongsong <joseph@josephmasongsong.com>
2019-04-25 14:27:39 -0700

    wknd unlimited changes

--- themes/weekend-unlimited-5a9778fb5ee54dbc96000000/instafeed.js
+++ themes/weekend-unlimited-5a9778fb5ee54dbc96000000/instafeed.js
@@ -1,8 +1,8 @@
 var userFeed = new Instafeed({
   get: 'user',
   userId: '7113559463',
-  clientId: '84d0e3742b684ed9a9ea7c45fc4618ef',
-  accessToken: '7113559463.1677ed0.62a7166b9de54c5d9001db77c34590cd',
+  clientId: '840f3abe69354e6698bbff73be5469f3',
+  accessToken: '7113559463.840f3ab.5faeb615439b4ba3a6124f8befdad63d',
   resolution: 'standard_resolution',
   template: '<a href="{{link}}" target="_blank" id="{{id}}" class="col-lg-2 col-md-4 col-xs-6"><div class="photo-container" style="background:url({{image}}) no-repeat center center; background-size:cover;"></div></a>',
   sortBy: 'most-recent',
